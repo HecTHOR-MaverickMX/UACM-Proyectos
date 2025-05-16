ca886205afe30f001d718dcfc75d4d0e35ba9245
HecTHOR-MaverickMX <152467854+HecTHOR-MaverickMX@users.noreply.github.com>
2025-05-16 11:00:28 -0600

    Calcular el cuadrado de un numero.cpp

--- Calcular el cuadrado de un numero.cpp
+++ Calcular el cuadrado de un numero.cpp
@@ -6,13 +6,6 @@
 #include <cstdlib>
 
 using namespace std;
-// Este programa calcula el cuadrado de un numero
-// y lo imprime en pantalla
-// El programa pide al usuario que ingrese un numero
-// y luego calcula el cuadrado de ese numero
-// El programa utiliza la funcion pow() de la libreria cmath
-// para calcular el cuadrado del numero
-
 
 int main() {
     
